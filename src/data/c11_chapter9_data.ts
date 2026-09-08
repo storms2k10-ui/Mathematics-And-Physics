1e4a15c6bc46a01879eabe2db953af13d2b35855
storms2k10-ui <storms2k10@gmail.com>
2026-09-08 20:37:03 +0500

    feat: expand chapter content with normal difficulty questions
    Integrated additional question sets for chapters 8 through 11 and updated question count metadata to reflect the expanded curriculum coverage.

--- src/data/c11_chapter9_data.ts
+++ src/data/c11_chapter9_data.ts
@@ -1,16 +1,29 @@
 import { Question } from '../types';
+import { C11_CH9_NORMAL_PART1 } from './c11_ch9_normal_part1';
+import { C11_CH9_NORMAL_PART2 } from './c11_ch9_normal_part2';
+import { C11_CH9_NORMAL_PART3 } from './c11_ch9_normal_part3';
+import { C11_CH9_NORMAL_PART4 } from './c11_ch9_normal_part4';
 import { C11_CH9_ADVANCED_PART1 } from './c11_ch9_advanced_part1';
 import { C11_CH9_ADVANCED_PART2 } from './c11_ch9_advanced_part2';
 import { C11_CH9_ADVANCED_PART3 } from './c11_ch9_advanced_part3';
 import { C11_CH9_ADVANCED_PART4 } from './c11_ch9_advanced_part4';
+
+export const C11_CH9_NORMAL_QUESTIONS: Question[] = [
+  ...C11_CH9_NORMAL_PART1, // 50 MCQs (Q1 - Q50)
+  ...C11_CH9_NORMAL_PART2, // 50 MCQs (Q51 - Q100)
+  ...C11_CH9_NORMAL_PART3, // 50 MCQs (Q101 - Q150)
+  ...C11_CH9_NORMAL_PART4, // 50 MCQs (Q151 - Q200)
+]; // Total: 200 Normal Questions
 
 export const C11_CH9_ADVANCED_QUESTIONS: Question[] = [
   ...C11_CH9_ADVANCED_PART1,
   ...C11_CH9_ADVANCED_PART2,
   ...C11_CH9_ADVANCED_PART3,
   ...C11_CH9_ADVANCED_PART4,
-];
+]; // Total: 200 Advanced Questions
 
 export const C11_CH9_QUESTIONS: Question[] = [
+  ...C11_CH9_NORMAL_QUESTIONS,
   ...C11_CH9_ADVANCED_QUESTIONS,
 ];
+
